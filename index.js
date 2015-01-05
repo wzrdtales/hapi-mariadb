b298a73aa2e0c6831888d2a77d353c7fce379d11
Tobias Gurtzick <magic@wizardtales.com>
2015-01-05 20:22:59 +0100

    add mariadb-transact as optionality

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 
 exports.register = function(plugin, options, next) {
-  var mariasql_pool = require('my_pool_sql');
+  
 
-  plugin.expose('pool', new mariasql_pool( options.connectionCount, options.mariasql ) );
+  if( options.useTransact )
+  {
+    var mariasql_trans = require("mariadb-transact");
+    plugin.expose('pool', new mariasql_trans( options.connectionCount ) );
+  }
+  else
+  {
+    var mariasql = require('my_pool_sql');
+    plugin.expose('pool', new mariasql_pool( options.connectionCount, options.mariasql ) );
+  }
+  
   next();
 };
 
